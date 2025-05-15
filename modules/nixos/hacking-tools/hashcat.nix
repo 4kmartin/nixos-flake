757003169bf0c27c9e688599636a7a78b532ce49
Anthany Martin <4kmartin@gmail.com>
2025-05-15 18:57:23 -0500

    trying to fix hashcat. it is not working

--- modules/nixos/hacking-tools/hashcat.nix
+++ modules/nixos/hacking-tools/hashcat.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{  pkgs, ... }:
 
 {
   #this module contains all of the extra packages and settings
@@ -6,12 +6,20 @@
 
   environment.systemPackages = with pkgs; [
     hashcat
+    hashcat-utils
   ];
 
   
-  hardware.graphics.extraPackages = with pkgs; [
-    intel-media-driver
-    intel-ocl
-  ];
-  # services.xserver.videoDrivers = [ "intel" ];
+  hardware.graphics = {
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-ocl
+      vpl-gpu-rt
+      vaapiIntel
+      intel-media-driver
+    ];
+    enable = true;
+    
+  };
+  services.xserver.videoDrivers = [ "intel" ];
 }
